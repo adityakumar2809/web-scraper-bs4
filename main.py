992273cbb3c2558ccd0734245a8065361e5f9044
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-06 03:07:49 +0530

    Get prices of courses along with their name

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def main():
         soup = BeautifulSoup(content, 'lxml')
         course_cards = soup.find_all('div', class_='card')
         for course_card in course_cards:
-            print(course_card.h5)
+            course_name = course_card.h5.text
+            course_price = course_card.a.text.split()[-1]
+            print(f'{course_name} costs {course_price}')
 
 
 if __name__ == "__main__":
