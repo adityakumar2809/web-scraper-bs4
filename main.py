cfa5e1e6088cab1b3ffcd83863b11ebad8dfb8ae
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-06 02:58:48 +0530

    Include .gitignore to exclude local configurations

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 
 with open('index.html', 'r') as html_file:
     content = html_file.read()
-    
+
     soup = BeautifulSoup(content, 'lxml')
     tags = soup.find_all('h5')
     print(tags)
